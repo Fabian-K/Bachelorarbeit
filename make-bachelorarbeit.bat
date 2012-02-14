707b56e8baf520e6830673be40112891c5b38e70
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-02-14 16:41:14 +0100

    autocommit

--- make-bachelorarbeit.bat
+++ make-bachelorarbeit.bat
@@ -9,5 +9,6 @@ echo creating git commit...
 "C:\Program Files (x86)\Git\bin\git.exe" config user.name "Fabian Kajzar"
 "C:\Program Files (x86)\Git\bin\git.exe" config user.email "fabiankajzar@googlemail.com"
 "C:\Program Files (x86)\Git\bin\git.exe" commit -a -m autocommit -e
-rem "C:\Program Files (x86)\Git\bin\git.exe" log -1 --name-status
+"C:\Program Files (x86)\Git\bin\git.exe" log -1 --name-status
+start se-pa2-vorlage.pdf
 pause
