460e411faebc791925fbd2acc6c123f2367979f9
unknown <d053397@WDFN00297075A.(none)>
2012-02-14 15:52:01 +0100

    automatic build commit

--- make-bachelorarbeit.bat
+++ make-bachelorarbeit.bat
@@ -1,6 +1,7 @@
 call make-projektarbeit.bat se-pa2-vorlage
+"C:\Program Files (x86)\Git\bin\git.exe" config --global user.name="Fabian Kajzar"
+"C:\Program Files (x86)\Git\bin\git.exe" config --global user.email="fabiankajzar@googlemail.com"
 "C:\Program Files (x86)\Git\bin\git.exe" status
 "C:\Program Files (x86)\Git\bin\git.exe" commit -m "automatic build commit" -a
-"C:\Program Files (x86)\Git\bin\git.exe" status
 "C:\Program Files (x86)\Git\bin\git.exe" log --name-status HEAD^..HEAD
 pause 
