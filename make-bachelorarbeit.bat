574b56e8b5d3af167e35dff9998753ea5c8408ab
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-02-14 16:49:35 +0100

    autocommit

--- make-bachelorarbeit.bat
+++ make-bachelorarbeit.bat
@@ -5,10 +5,10 @@ cls
 echo cleaning up...
 call cleanup.bat
 cls
+start se-pa2-vorlage.pdf
 echo creating git commit...
 "C:\Program Files (x86)\Git\bin\git.exe" config user.name "Fabian Kajzar"
 "C:\Program Files (x86)\Git\bin\git.exe" config user.email "fabiankajzar@googlemail.com"
 "C:\Program Files (x86)\Git\bin\git.exe" commit -a -m autocommit -e
 "C:\Program Files (x86)\Git\bin\git.exe" log -1 --name-status
-start se-pa2-vorlage.pdf
 pause
