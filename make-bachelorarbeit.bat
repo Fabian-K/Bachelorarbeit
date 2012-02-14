2bae3d495d8578f4d3388259fccdfb9d122068e0
unknown <d053397@WDFN00297075A.(none)>
2012-02-14 15:35:16 +0100

    automatic build commit

--- make-bachelorarbeit.bat
+++ make-bachelorarbeit.bat
@@ -1,1 +1,5 @@
-make-projektarbeit.bat se-pa2-vorlage
+call make-projektarbeit.bat se-pa2-vorlage
+"C:\Program Files (x86)\Git\bin\git.exe" status
+"C:\Program Files (x86)\Git\bin\git.exe" commit -m "automatic build commit" -a
+"C:\Program Files (x86)\Git\bin\git.exe" status
+pause
--- make-projektarbeit.bat
+++ make-projektarbeit.bat
@@ -1,6 +1,3 @@
-@echo off
-echo Uebersetzung einer tex-datei
-echo Parameter: Name der tex-datei OHNE die Endung .tex
 pdflatex %1.tex
 bibtex %1
 makeindex -s %1.ist -t %1.alg -o %1.acr %1.acn
@@ -8,5 +5,4 @@ makeindex -s %1.ist -t %1.glg -o %1.gls %1.glo
 makeindex -s %1.ist -t %1.slg -o %1.syi %1.syg
 pdflatex %1.tex
 pdflatex %1.tex
-pdflatex %1.tex
-
+pdflatex %1.tex
