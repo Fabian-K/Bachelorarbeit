c70d06ba98bc8d376b9f12a01e4fc6133195b047
unknown <d053397@WDFN00297075A.(none)>
2012-02-14 15:41:41 +0100

    automatic build commit

--- make-bachelorarbeit.bat
+++ make-bachelorarbeit.bat
@@ -1,5 +1,4 @@
 call make-projektarbeit.bat se-pa2-vorlage
-"C:\Program Files (x86)\Git\bin\git.exe" status
 "C:\Program Files (x86)\Git\bin\git.exe" commit -m "automatic build commit" -a
 "C:\Program Files (x86)\Git\bin\git.exe" status
 pause
