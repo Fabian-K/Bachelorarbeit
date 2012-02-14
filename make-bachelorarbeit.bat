8a3f42e37fa3723806cc1bbc7e11a5c2d4f62eea
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-02-14 16:09:35 +0100

    automatic build commit

--- make-bachelorarbeit.bat
+++ make-bachelorarbeit.bat
@@ -2,6 +2,9 @@
 echo compiling latex...
 call make-projektarbeit.bat se-pa2-vorlage
 cls
+echo cleaning up...
+call cleanup.bat
+cls
 echo creating git commit...
 "C:\Program Files (x86)\Git\bin\git.exe" config user.name "Fabian Kajzar"
 "C:\Program Files (x86)\Git\bin\git.exe" config user.email "fabiankajzar@googlemail.com"
