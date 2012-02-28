f4568cc1d3bfaa9918b8e3fb19da9996704f0526
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-02-28 09:39:34 +0100

    autocommit

--- make-bachelorarbeit-commit.bat
+++ make-bachelorarbeit-commit.bat
@@ -1,6 +1,6 @@
 @echo off
 echo compiling latex...
-call make-projektarbeit.bat se-pa2-vorlage
+call make-projektarbeit.bat bachelorarbeit
 cls
 echo cleaning up...
 call cleanup.bat
@@ -9,5 +9,5 @@ cls
 "C:\Program Files (x86)\Git\bin\git.exe" config user.email "fabiankajzar@googlemail.com"
 "C:\Program Files (x86)\Git\bin\git.exe" commit -a -m autocommit
 "C:\Program Files (x86)\Git\bin\git.exe" log -1 --name-status
-start se-pa2-vorlage.pdf
+start bachelorarbeit.pdf
 pause
--- make-bachelorarbeit-evaluate.bat
+++ make-bachelorarbeit-evaluate.bat
@@ -1,12 +1,12 @@
 @echo off
 echo compiling latex...
-call make-projektarbeit.bat se-pa2-vorlage
+call make-projektarbeit.bat bachelorarbeit
 cls
 echo cleaning up...
 call cleanup.bat
 cls
 echo please review changes and close the pdf to commit or reset changes
-"C:\Program Files (x86)\Adobe\Reader 10.0\Reader\AcroRd32.exe" se-pa2-vorlage.pdf
+"C:\Program Files (x86)\Adobe\Reader 10.0\Reader\AcroRd32.exe" bachelorarbeit.pdf
 echo creating git commit...
 "C:\Program Files (x86)\Git\bin\git.exe" config user.name "Fabian Kajzar"
 "C:\Program Files (x86)\Git\bin\git.exe" config user.email "fabiankajzar@googlemail.com"
