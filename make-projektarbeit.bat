ece977420de6581628b67820295977a895d7429e
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-03-15 17:02:44 +0100

    autocommit

--- make-projektarbeit.bat
+++ make-projektarbeit.bat
@@ -5,4 +5,11 @@ makeindex -s %1.ist -t %1.glg -o %1.gls %1.glo
 makeindex -s %1.ist -t %1.slg -o %1.syi %1.syg
 pdflatex %1.tex
 pdflatex %1.tex
+pdflatex %1.tex
+bibtex %1
+makeindex -s %1.ist -t %1.alg -o %1.acr %1.acn
+makeindex -s %1.ist -t %1.glg -o %1.gls %1.glo
+makeindex -s %1.ist -t %1.slg -o %1.syi %1.syg
+pdflatex %1.tex
+pdflatex %1.tex
 pdflatex %1.tex
